dd39c05bba1035125682caaaab5d4396d3d878fa
Nelbi Leon <nelbi.leon@gmail.com>
2025-05-09 14:34:14 +0200

    ex01: TO_TEST

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -16,8 +16,65 @@ void printHeader(const std::string& str)
 	std::cout << RESET << std::endl;
 }
 
-int main(void)
-{
+// int main(void)
+// {
+//     {
+//         printHeader("Test: std::string");
 
-    return 0;
+//         std::string array[5] = {"Test1", "Test2", "Test3", "Test4", "Test5"};
+//         ::iter(array, 5, printElement<std::string>);
+//     }
+//     {
+//         printHeader("Test: int");
+
+//         int array[5] = {1, 2, 3, 4 ,5};
+//         ::iter(array, 5, printElement<int>);
+
+//     }
+//     {
+//         printHeader("Test: pointer");
+
+//         char a = 'a';
+//         char b = 'b';
+//         char c = 'c';
+//         char d = 'd';
+//         char e = 'e';
+
+//         char *array[5] = {&a, &b, &c, &d ,&e};
+
+//         ::iter(array, 5, printElement<void *>);
+
+//     }
+
+//     return 0;
+// }
+
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
+}
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
+
+  iter( tab, 5, print<const int> );
+  iter( tab2, 5, print<Awesome> );
 }
